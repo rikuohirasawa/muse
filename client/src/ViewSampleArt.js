dd755088f9f0da38c47a24d8b31cde8057e4b7bc
Rikuo Hirasawa <rikuoh84@gmail.com>
2022-08-26 14:24:37 -0230

    sample setup contd

--- client/src/ViewSampleArt.js
+++ client/src/ViewSampleArt.js
@@ -3,41 +3,59 @@ import { Swiper, SwiperSlide } from 'swiper/react'
 import 'swiper/swiper.min.css'
 
 
-import { useEffect, useContext } from "react"
+import { useEffect, useContext, useState } from "react"
 
 import { ArtContext } from "./ArtContext"
 
 export const ViewSampleArt = () => {
-    const { dispatch, sampleArt } = useContext(ArtContext);
+    // local state to prevent re-render when context updates
+    const [sampleArt, setSampleArt] = useState([]);
     
     useEffect(()=>{
         fetch('/api/sample-art')
             .then(res=>res.json())
             .then((data)=>{
-                dispatch({type: 'set-sample-art-data', sampleArt: data.data.data})
+                console.log('run')
+                setSampleArt(data.data)
             })
     }, []);
-
     if (sampleArt.length > 0) {
         return (
             <>
-            <Swiper>
+            <Swiper slidesPerView={1}>
                 {sampleArt.map(element=>{
                     return (
-                        <SwiperSlide>
-                            <img src={`https://www.artic.edu/iiif/2/${element.image_id}/full/843,/0/default.jpg`}/>
+                        <SwiperSlide style={{
+                            display: 'flex', 
+                            flexDirection: 'column',
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                            border: '1px solid red',
+                            height: '100vh'
+                            }}>
+                            <Image src={`https://www.artic.edu/iiif/2/${element[0].image_id}/full/843,/0/default.jpg`} alt={element[0].thumbnail && element[0].thumbnail.alt_text}/>
+                            <InformationContainer>
+                            {element[0].artist_titles.length === 0 ? 
+                                <ArtistName>Unknown</ArtistName>
+                            :
+                            element[0].artist_titles.map(e=>{
+                                return <ArtistName>{e}</ArtistName>
+                            })}
+                            </InformationContainer>
                         </SwiperSlide>
-
                     )
                 })}
-                <SwiperSlide>
-
-                </SwiperSlide>
-            </Swiper>
-           
+            </Swiper>       
             </>
-
         )
     }
-
 }
+
+
+const Image = styled.img`
+    max-height: 80vh;
+`
+
+const InformationContainer = styled.div``
+
+const ArtistName = styled.div``
--- server/handlers/sampleArt.js
+++ server/handlers/sampleArt.js
@@ -1,20 +1,29 @@
 const request = require('request-promise');
 
 const getSampleArt = async (req, res) => {
-    const randomPage = Math.floor(Math.random() * 500 + 1)
-    const options = {
-        uri: `https://api.artic.edu/api/v1/artworks?page=${randomPage}&limit=50&fields=id,title,artist_titles,category_titles,date_display,place_of_origin,alt_text,image_id`,
-        headers: {
-            'Accept': 'application/json'
-        }
-    }
     try {
-        const result = await JSON.parse(await request(options))
-        console.log(result)
-        if (result.data.length > 0) {
+        // loop to create array of random art pieces
+        // without the loop, this will return a random page, however pages are formatted in a way that
+        // pieces are grouped by collections, so getting a random page will often times return
+        // an array of art that are very similar - array is limited to length of 8 as API throttles
+        // at 60 calls/minute (loop is making 8 individual calls)
+        let getRandomArtArray = [];
+        for (let i=0; i <8; i++) {
+            const randomPage = Math.floor(Math.random() * 8000 + 1)
+            const options = {
+                uri: `https://api.artic.edu/api/v1/artworks?page=${randomPage}&limit=1&fields=id,title,artist_titles,category_titles,date_display,place_of_origin,image_id,thumbnail`,
+                headers: {
+                    'Accept': 'application/json'
+                }
+            }
+            const result = (await JSON.parse(await request(options)))
+            getRandomArtArray.push(result.data)
+        }
+        if (getRandomArtArray.length > 0) {
+            console.log(getRandomArtArray)
             res.status(200).json({
                 status: 200,
-                data: result
+                data: getRandomArtArray
             })
         } else {
             res.status(400).json({
