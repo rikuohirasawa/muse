58003e46d6428fc9fff8dd6c38bbcbf8c397b75a
Rikuo Hirasawa <rikuoh84@gmail.com>
2022-08-26 12:50:54 -0230

    swiper setup

--- client/src/ViewSampleArt.js
+++ client/src/ViewSampleArt.js
@@ -1,14 +1,43 @@
-import { useEffect } from 'react'
-import styled from 'styled-components'
+import styled from "styled-components"
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/swiper.min.css'
+
+
+import { useEffect, useContext } from "react"
+
+import { ArtContext } from "./ArtContext"
 
 export const ViewSampleArt = () => {
+    const { dispatch, sampleArt } = useContext(ArtContext);
+    
     useEffect(()=>{
         fetch('/api/sample-art')
             .then(res=>res.json())
-            .then(data=>console.log(data))
-    }, [])
-    return (
-        <div>hello</div>
-    )
+            .then((data)=>{
+                dispatch({type: 'set-sample-art-data', sampleArt: data.data.data})
+            })
+    }, []);
+
+    if (sampleArt.length > 0) {
+        return (
+            <>
+            <Swiper>
+                {sampleArt.map(element=>{
+                    return (
+                        <SwiperSlide>
+                            <img src={`https://www.artic.edu/iiif/2/${element.image_id}/full/843,/0/default.jpg`}/>
+                        </SwiperSlide>
+
+                    )
+                })}
+                <SwiperSlide>
+
+                </SwiperSlide>
+            </Swiper>
+           
+            </>
+
+        )
+    }
 
 }
--- client/src/index.js
+++ client/src/index.js
@@ -5,11 +5,11 @@ import { ArtContextProvider } from './ArtContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+
     <ArtContextProvider>
       <App />
     </ArtContextProvider>
-  </React.StrictMode>
+
 );
 
 
