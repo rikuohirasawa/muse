666bf3443ef77fff76b86f6175288141bbc8e2a4
Rikuo Hirasawa <rikuoh84@gmail.com>
2022-08-28 12:41:45 -0230

    artist-details setup

--- client/src/ArtistDetails.js
+++ client/src/ArtistDetails.js
@@ -14,23 +14,25 @@ export const ArtistDetails = () => {
         // to work around this, the artwork ids returned from the first fetch are pushed to an
         // array, and then used to in a second fetch to retrieve images
         let idArray = []
+        let idk = ''
+
         fetch(`https://api.artic.edu/api/v1/artworks/search?q=${artistName}`)
         .then(res=>res.json())
         .then(data=>{
-
             data.data.forEach(e=>{
                 idArray.push(e.id)
             })
-            console.log(idArray)
             console.log(data.data)
         }).then(
-            fetch(`https://api.artic.edu/api/v1/artworks?q=${artistName}ids=${idArray}`)
+            fetch(`https://api.artic.edu/api/v1/artworks?ids=${idArray.join(',')}
+            &fields=title,id,image_id,date_display,category_titles`)
             .then(res=>res.json())
             .then(data=>{
+                console.log(idArray.join(','))
+                console.log(data)
                 setArtistInfo(data.data)
             })
         )
-
     }, [])
 
     if (artistInfo) {
@@ -39,11 +41,15 @@ export const ArtistDetails = () => {
                 <h1>{artistName}</h1>
                 <Line/>
                 <Content>
-                    {artistInfo.map(e=>{
+                    {artistInfo.map(element=>{
                         return (
                             <ArtPieceContainer>
-        
-
+                                <Image src={`https://www.artic.edu/iiif/2/${element.image_id}/full/843,/0/default.jpg`}/>
+                                <div>{element.title} ({element.date_display})</div>
+                                <div>{element.category_titles.map(e=>{
+                                    return <span className='categories'> {e} </span>
+                                })}</div>
+                                <Line/>
                             </ArtPieceContainer>             
                         )
                     })}
@@ -51,10 +57,29 @@ export const ArtistDetails = () => {
                 </Content>
             </>
         )
+    } else {
+        return (
+            <div> LOADING </div>
+        )
     }
 
 }
 
 const Content = styled.div``
 
-const ArtPieceContainer = styled.div``
+const ArtPieceContainer = styled.div`
+width: 400px;
+display: flex;
+flex-direction: column;
+align-items: center;
+gap: 8px;
+
+.categories {
+    :not(:last-child) {
+        border-right: 1px solid rgba(255, 255, 255, 0.3);
+        margin-right: 5px;
+    }
+}`
+
+const Image = styled.img`
+width: 100%;`
